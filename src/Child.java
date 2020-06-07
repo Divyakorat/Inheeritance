5914e64a7dc18c3ae96a532dee0ca85bb4d0794d
divya <divyakorat@gmail.com>
2020-06-07 11:16:22 +0100

    Inheritance

---
+++ src/Child.java
@@ -0,0 +1,15 @@
+public class Child  extends Parent{
+    void sub (){
+        double ans = a-b;
+        System.out.println("subtraction is " +ans);
+    }
+
+    public static void main(String[] args) {
+        Child child = new Child();
+        child.sub();
+        child.add();
+
+
+    }
+
+}
---
+++ src/Parent.java
@@ -0,0 +1,8 @@
+public class Parent {
+    double a = 10;
+    double b = 20;
+    void add (){
+        double ans = a+b;
+        System.out.println("addition is "+ans);//concatination
+    }
+}
